c4cbab97bae2b1e400cc1b262021c349b3c60b2f
Akshay <akshay.pandey2021@vitbhopal.ac.in>
2024-06-02 03:36:24 +0530

    features section

--- ecommerce/tailwind.config.js
+++ ecommerce/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "shadow-color": "rgba(0, 0, 0, 0.05)",
+        "on-hover-shadow": "rgba(0, 0, 0, 0.12)",
+      },
+    },
   },
   plugins: [],
 };
