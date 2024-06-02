f20743b3c23c26aeeaa4a2b49d7084f9683cdc10
Akshay <akshay.pandey2021@vitbhopal.ac.in>
2024-06-03 03:41:24 +0530

    completed basic tailwind ecommerce website

--- ecommerce/tailwind.config.js
+++ ecommerce/tailwind.config.js
@@ -6,6 +6,11 @@ module.exports = {
       colors: {
         "shadow-color": "rgba(0, 0, 0, 0.05)",
         "on-hover-shadow": "rgba(0, 0, 0, 0.12)",
+        "on-card-shadow": "rgba(0, 0, 0, 0.02)",
+        "on-card-hover": "rgba(0,0,0,0.06)",
+        "card-font": "rgba(41, 41, 41)",
+        "lower-banner-txt": "rgba(248, 248, 248)",
+        "footer-text": "rgb(109, 109, 109)",
       },
     },
   },
